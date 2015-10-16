1ee11118830ff824b590ba25dc2324b3a3e060d1
Kim Dilling <kimdilling@gmail.com>
2015-10-15 21:38:18 -0500

    Initial db setup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', dogs: [] });
+  res.render('index', { title: 'Space Harmony', name: [] });
 });
 
-/* POST when the user "likes" a new Corgi. */
+/* POST when the user "likes" a new Spacer. */
 router.post('/likes', function(req, res, next) {
+	//if like = true {
+		//name.collection.prepend({});
 
+	}
 });
 
 module.exports = router;
